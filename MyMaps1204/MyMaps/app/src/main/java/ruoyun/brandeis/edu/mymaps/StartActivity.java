237e5f334e796fa15a80d411fe74abcacd8e7061
jasminesong <2837320441@qq.com>
2015-12-04 06:46:46 -0500

    first commit

---
+++ MyMaps1204/MyMaps/app/src/main/java/ruoyun/brandeis/edu/mymaps/StartActivity.java
@@ -0,0 +1,69 @@
+package ruoyun.brandeis.edu.mymaps;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class StartActivity extends AppCompatActivity {
+
+    //added by, used for quit app
+    public static boolean terminatorFlag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        //modified by
+        Button btn5 = (Button) findViewById(R.id.button5);
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent my_intent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(my_intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //added by
+    @Override
+     public void onRestart() {
+        if(true == terminatorFlag) {
+            this.finish();
+        }
+
+        super.onRestart();
+    }
+}
